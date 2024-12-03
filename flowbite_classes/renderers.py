50e8c8d5d5f4d1ef16331ae0b998db10ec639511
Pere Picornell <pere@perepicornell.com>
2024-12-03 12:02:18 +0100

    Fields template renamed.

--- flowbite_classes/renderers.py
+++ flowbite_classes/renderers.py
@@ -2,4 +2,4 @@ from django.forms.renderers import TemplatesSetting
 
 
 class CustomFormRenderer(TemplatesSetting):
-    field_template_name = "flowbite_classes/fields/field_default.html"
+    field_template_name = "flowbite_classes/fields/fields.html"
